db48c4f0292aa1bf741e5de25a4d208c7ae644a8
Ajeet Ganga <aganga@ebay.com>
2014-09-12 23:27:23 +0000

    Added install_hava7_ubuntu.sh

---
+++ install_java7_ubuntu.sh
@@ -0,0 +1,9 @@
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java7-installer
+
+#t'll keep your java 7 installation up to date.
+
+#to automatically set up the Java 7 environment variables JAVA_HOME and PATH:
+
+sudo apt-get install oracle-java7-set-default
