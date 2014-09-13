b5968c4d97e9ac17890c30d143afa64411979b2b
ajeet <toajeet@yahoo.com>
2014-09-12 18:19:58 -0700

    Update install_java7_ubuntu.sh

--- install_java7_ubuntu.sh
+++ install_java7_ubuntu.sh
@@ -1,3 +1,4 @@
+# From : http://stackoverflow.com/questions/16263556/installing-java-7-on-ubuntu
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install oracle-java7-installer
@@ -7,3 +8,7 @@ sudo apt-get install oracle-java7-installer
 #to automatically set up the Java 7 environment variables JAVA_HOME and PATH:
 
 sudo apt-get install oracle-java7-set-default
+
+# If this doesn't work follow this
+# http://whatizee.blogspot.com/2014/07/solved-e-unable-to-locate-package.html
+
