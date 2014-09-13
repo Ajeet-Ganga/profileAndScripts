1d66b5898bb55b46be48069dd3f8628a8ad2319d
Ganga <aganga@memcached1-177037.phx-os1.stratus.dev.ebay.com>
2014-09-12 17:54:03 -0700

    added cassandra installation script

---
+++ install_cassandra_ubuntu.sh
@@ -0,0 +1,15 @@
+cd ~/temp
+wget http://www.us.apache.org/dist/cassandra/2.1.0/apache-cassandra-2.1.0-bin.tar.gz
+tar -xvzf aapache-cassandra-2.1.0-bin.tar.gz
+mv apache-cassandra-2.1.0-bin.tar.gz ~/cassandra
+
+sudo mkdir /var/lib/cassandra
+sudo mkdir /var/log/cassandra
+sudo chown -R $USER:$GROUP /var/lib/cassandra
+sudo chown -R $USER:$GROUP /var/log/cassandra
+
+export CASSANDRA_HOME=~/cassandra
+echo 'export CASSANDRA_HOME=~/cassandra' >> ~/.bashrc
+export PATH=$PATH:$CASSANDRA_HOME/bin
+echo 'export PATH=$PATH:$CASSANDRA_HOME/bin' >> ~/.bashrc
+
