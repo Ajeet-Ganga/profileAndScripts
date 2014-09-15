01c17d153d88bda36be5e1829d5c3c2769f31fd3
ajeet <toajeet@yahoo.com>
2014-09-15 15:30:54 -0700

    Update install_java7_ubuntu.sh

--- install_java7_ubuntu.sh
+++ install_java7_ubuntu.sh
@@ -12,3 +12,22 @@ sudo apt-get install oracle-java7-set-default
 # If this doesn't work follow this
 # http://whatizee.blogspot.com/2014/07/solved-e-unable-to-locate-package.html
 
+
+# Using  DataStax
+
+#if [ "$(id -u)" != "0" ]; then
+#	echo "Sorry, retry as Root (sudo ...)"
+	#exit 1
+#fi
+#
+#sudo sh -c 'echo "deb http://debian.datastax.com/community stable main" > /etc/apt/sources.list.d/cassandra.sources.list'
+#curl -L http://debian.datastax.com/debian/repo_key | sudo apt-key add -
+#sudo apt-get update
+#sudo apt-get install dsc12=1.2.10-1 cassandra=1.2.10
+
+#sudo service cassandra start
+#sudo service cassandra stop
+#sudo rm -rf /var/lib/cassandra/data/system/*
+
+
+
