8670d91ba478883ed74b230e3794036bd2d4c45f
ajeet <toajeet@yahoo.com>
2014-09-15 16:50:19 -0700

    Update install_cassandra_ubuntu.sh

--- install_cassandra_ubuntu.sh
+++ install_cassandra_ubuntu.sh
@@ -1,3 +1,29 @@
+###################################################
+# Using  DataStax
+################################################### 
+
+if [ "$(id -u)" != "0" ]; then
+	echo "Sorry, retry as Root (sudo ...)"
+	exit 1
+fi
+
+sudo sh -c 'echo "deb http://debian.datastax.com/community stable main" > /etc/apt/sources.list.d/cassandra.sources.list'
+curl -L http://debian.datastax.com/debian/repo_key | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install dsc12=1.2.10-1 cassandra=1.2.10
+
+#sudo service cassandra start	
+#sudo service cassandra stop
+#sudo rm -rf /var/lib/cassandra/data/system/*
+
+exit 0;
+
+
+###################################################
+# Using  Binary from cassandra distribution
+################################################### 
+
+
 cd /tmp
 wget http://www.us.apache.org/dist/cassandra/2.1.0/apache-cassandra-2.1.0-bin.tar.gz
 tar -xvzf apache-cassandra-2.1.0-bin.tar.gz
@@ -20,28 +46,3 @@ alias cassandra_stop='sudo pkill -f CassandraDaemon'
 echo 'alias cassandra_start_bg="sudo sh ~/cassandra/bin/cassandra"' >> ~/.bashrc
 echo 'alias cassandra_start_fg="sudo sh ~/cassandra/bin/cassandra -f"' >> ~/.bashrc
 echo 'alias cassandra_stop="sudo pkill -f CassandraDaemon"' >> ~/.bashrc
-
-
-
-
-
-# Using  DataStax
-
-#if [ "$(id -u)" != "0" ]; then
-#	echo "Sorry, retry as Root (sudo ...)"
-#	exit 1
-#fi
-#
-#sudo sh -c 'echo "deb http://debian.datastax.com/community stable main" > /etc/apt/sources.list.d/cassandra.sources.list'
-#curl -L http://debian.datastax.com/debian/repo_key | sudo apt-key add -
-#sudo apt-get update
-#sudo apt-get install dsc12=1.2.10-1 cassandra=1.2.10
-
-#sudo service cassandra start
-#sudo service cassandra stop
-#sudo rm -rf /var/lib/cassandra/data/system/*
-
-
-
-
-
