28de78c5761ae99fb97e3c4589cb81b9afa9013c
ajeet <toajeet@yahoo.com>
2017-03-16 18:58:36 -0700

    Update docker.bash

--- install/ubuntu/docker.bash
+++ install/ubuntu/docker.bash
@@ -1,5 +1,4 @@
 sudo apt-get update && sudo apt-get upgrade
-sudo apt-get dist-upgrade
 sudo apt-get install  linux-image-extra-$(uname -r)  linux-image-extra-virtual
 sudo apt-get install     apt-transport-https     ca-certificates     curl     software-properties-common
 sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
